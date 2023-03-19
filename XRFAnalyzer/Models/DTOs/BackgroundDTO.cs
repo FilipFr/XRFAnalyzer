382e2eff34d5864e234533e12ad2c7a7570d6ab4
FilipFr <97771@stuba.sk>
2023-03-19 20:17:26 +0100

    Added BackgroundDTO

---
+++ XRFAnalyzer/Models/DTOs/BackgroundDTO.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XRFAnalyzer.Models.DTOs
+{
+    internal partial class BackgroundDTO : ObservableObject
+    {
+        [ObservableProperty]
+        private List<int>? _counts;
+        [ObservableProperty]
+        private int _lambda;
+        [ObservableProperty]
+        private int _iterationCount;
+
+        public BackgroundDTO() 
+        {
+            _counts = new List<int>();
+        }
+    }
+}
